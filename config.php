26626197c1a96435c005163ca08ec41d58ef7b11
siayi <ayisumarna@gmail.com>
2016-06-20 11:09:49 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.1.1/oc/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.1.1/oc/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/oc/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/html/oc/upload/system/');
+define('DIR_IMAGE', '/var/www/html/oc/upload/image/');
+define('DIR_LANGUAGE', '/var/www/html/oc/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/oc/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/oc/upload/system/config/');
+define('DIR_CACHE', '/var/www/html/oc/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/oc/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/oc/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/oc/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/oc/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'ciburial2014');
+define('DB_DATABASE', 'pasaamyi_ocar687');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
